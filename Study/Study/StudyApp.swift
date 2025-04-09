b34771e805827e23e27fb9c7bf18d914fc07ba04
Kostiantyn Vasyliev <kostiantyn.vasyliev@nixs.com>
2025-04-09 11:07:05 +0200

    Init project

---
+++ Study/Study/StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyApp.swift
+//  Study
+//
+//  Created by Kostiantyn Vasyliev on 2025. 04. 09..
+//
+
+import SwiftUI
+
+@main
+struct StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
