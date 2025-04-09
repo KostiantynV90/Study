2fd1d6c7b1aae6abe5e03aa6686e993b192c63e2
Kostiantyn Vasyliev <kostiantyn.vasyliev@nixs.com>
2025-04-09 11:26:17 +0200

    Create codeql.yml

--- Study/Study/ContentView.swift
+++ Study/Study/ContentView.swift
@@ -6,14 +6,59 @@
 //
 
 import SwiftUI
+import Combine
+
+let password = "my_secret_password_123"
+
+class ContentViewModel: ObservableObject {
+    @Published var isLoggedIn = false
+    @Published var loginText: String = ""
+    @Published var passText: String = ""
+    
+    
+    func checkPassword() {
+        
+        if passText == password {
+            print("OLOLO")
+        }
+        
+        let number: UInt32 = 123456
+        let unsafeValue: Float = unsafeBitCast(number, to: Float.self)
+        print(unsafeValue)
+    }
+}
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ContentViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, worldsefdsf!")
+            TextField("Login", text: $viewModel.loginText)
+                .padding(4)
+                .font(.headline)
+                .foregroundStyle(.black)
+                .border(Color.blue, width: 2)
+            TextField("Password", text: $viewModel.passText)
+                .padding(4)
+                .font(.headline)
+                .foregroundStyle(.black)
+                .border(Color.blue, width: 2)
+            Button {
+                viewModel.checkPassword()
+            } label: {
+                Text("Submit")
+                    .padding()
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .background(.blue)
+                    .cornerRadius(20)
+            }
+
         }
         .padding()
     }
